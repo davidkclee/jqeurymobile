62ca900578c381cd58f98ee53a8afc57774da9f9
davidkclee <davidkclee1984@gmail.com>
2014-03-04 18:09:55 +0800

    changes

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+	document.addEventListener("backbutton", onBackKeyDown, false);
+    document.addEventListener("menubutton", onMenuKeyDown, false);
+	
+    $("#btnEnter").click( function(){
+		alert("hi");
+	});
+});
+
+function onBackKeyDown() {
+    // your code..
+    alert('Back Button pressed..');
+}
+function onMenuKeyDown() {
+	// your code..
+	alert('Menu Button pressed..');
+}
